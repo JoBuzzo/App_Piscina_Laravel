4eb0cbef14a11d3ed83fa14d3f879bc75ed89167
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-11-19 11:52:05 -0300

    correcao da validacao unique

--- app/Http/Requests/ReservaFormRequest.php
+++ app/Http/Requests/ReservaFormRequest.php
@@ -26,7 +26,7 @@ class ReservaFormRequest extends FormRequest
         $id = $this->id ?? '';
         return [
                 'nome' => 'required|string|min:4|max:30',        
-                'primeiro_dia' => "required|date|unique:reservas,primeiro_dia,{$id},id",        
+                'primeiro_dia' => "required|date|unique:reservas,primeiro_dia,{$id},id|unique:reservas,ultimo_dia,{$id},id",        
                 // 'primeiro_dia' => "required|date|unique:reservas,ultimo_dia,{$id},id",        
                 'ultimo_dia' => "nullable|date|unique:reservas,primeiro_dia,{$id},id",        
                 'pagamento' => 'required',        
