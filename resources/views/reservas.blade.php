4fd9ad00188a7c3cd4000e1993316439bf74a0d7
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-12-05 00:00:00 -0300

    tabela

--- resources/views/reservas.blade.php
+++ resources/views/reservas.blade.php
@@ -7,7 +7,7 @@
 
     <div class="main-secundaria">
         <div class="table w-3/4 sm:rounded-lg space-y-5">
-            <div class="flex justify-between items-center pb-4">
+            <div class="flex justify-between items-center pb-4 space-x-2">
                 <div>
                     <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5" type="button">
                         <svg class="mr-2 w-4 h-4 text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
@@ -57,7 +57,7 @@
                 </div>
                 <div>
                     <a href="{{route('reservas.adicionar')}}"
-                    class="bg-gray-100 text-gray-500 text-sm font-bold whitespace-nowrap mr-2 px-2.5 py-2 rounded nao-importante">
+                    class="bg-gray-100 text-gray-500 text-sm font-bold whitespace-nowrap mr-2 px-2.5 py-2 rounded mobile-table">
                         <i class="fas fa-calendar-plus"></i> Adicionar
                     </a>
                 </div>
@@ -69,9 +69,9 @@
                     </form>
                 </div>
                 
-                <div>
-                    <span class="bg-green-100 text-green-500 text-sm font-bold whitespace-nowrap mr-2 px-2.5 py-1 rounded nao-importante">Pago</span>
-                    <span class="bg-gray-100 text-gray-500 text-sm font-bold whitespace-nowrap mr-2 px-2.5 py-1 rounded nao-importante">Em haver</span>
+                <div class="flex">
+                    <span class="bg-green-100 text-green-500 text-sm font-bold whitespace-nowrap mx-2 px-2.5 py-1 rounded mobile-table-valor">Pago</span>
+                    <span class="bg-gray-100 text-gray-500 text-sm font-bold whitespace-nowrap mx-2 px-2.5 py-1 rounded mobile-table-valor">Em haver</span>
                 </div>
 
             </div>
@@ -83,17 +83,17 @@
                         <th scope="col" class="py-3 px-6">
                             Datas
                         </th>
-                        <th scope="col" class="nao-importante py-3 px-6">
+                        <th scope="col" class="mobile-table py-3 px-6">
                             Nomes
                         </th>
-                        <th scope="col" class="nao-importante py-3 px-6">
-                            Valor Total
+                        <th scope="col" class="mobile-table-valor py-3 px-6">
+                            Preço
                         </th>
-                        <th scope="col" class="nao-importante py-3 px-6">
+                        <th scope="col" class="mobile-table py-3 px-6">
                             Pagou
                         </th>
-                        <th scope="col" class="nao-importante py-3 px-6">
-                            Quanto falta
+                        <th scope="col" class="mobile-table-valor py-3 px-6">
+                            Faltam
                         </th>
                         <th scope="col" class="py-3 px-2 text-center">
                             Consultar
@@ -112,16 +112,16 @@
                                 {{ date('d/m/Y', strtotime($reserva->primeiro_dia)) }}
                             @endif
                         </td>
-                        <td class="nao-importante py-4 px-6">
+                        <td class="mobile-table py-4 px-6">
                             {{$reserva->nome}}
                         </td>
-                        <td class="nao-importante py-4 px-6 text-green-500">
+                        <td class="mobile-table-valor py-4 px-6 text-green-500">
                             R${{$reserva->valor_total}}
                         </td>
-                        <td class="nao-importante py-4 px-6 @if ($reserva->valor_pago === $reserva->valor_total) text-green-500 @else text-yellow-500 @endif">
+                        <td class="mobile-table py-4 px-6 @if ($reserva->valor_pago === $reserva->valor_total) text-green-500 @else text-yellow-500 @endif">
                             R${{$reserva->valor_pago}}
                         </td>
-                        <td class="nao-importante py-4 px-6 @if ($reserva->valor_pendente > 0) text-red-500 @else text-green-500 @endif">
+                        <td class="mobile-table-valor py-4 px-6 @if ($reserva->valor_pendente > 0) text-red-500 @else text-green-500 @endif">
                             R${{$reserva->valor_pendente}}
                         </td>
                     
