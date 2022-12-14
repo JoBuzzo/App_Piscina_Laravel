3a866fa2ea150bd5b9b3977913efb27d1c1d4213
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-12-14 11:22:59 -0300

    admins no dropdown

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -11,18 +11,21 @@
             <div class="font-medium truncate">{{ Auth::user()->name }}</div>
             </div>
             <ul class="py-1 text-sm" aria-labelledby="dropdownUserAvatarButton">
-            <li>
-                <a href="{{ route('index') }}" class="block py-2 px-4 hover:bg-gray-100"><i class="fas fa-chart-bar"></i> Dashboard</a>
-            </li>
-            <li>
-                <a href="{{ route('perfil', ['id' => Auth::user()->id]) }} " class="block py-2 px-4 hover:bg-gray-100"><i class="fa fa-user"></i> Perfil</a>
-            </li>
-            <li>
-                <a href="{{ route('viewConfig') }}" class="block py-2 px-4 hover:bg-gray-100"><i class="fas fa-cog"></i> Configurações</a>
-            </li>
+                <li>
+                    <a href="{{ route('index') }}" class="block py-2 px-4 hover:bg-gray-100"><i class="fas fa-chart-bar"></i> Dashboard</a>
+                </li>
+                <li>
+                    <a href="{{ route('perfil', ['id' => Auth::user()->id]) }} " class="block py-2 px-4 hover:bg-gray-100"><i class="fas fa-user-circle"></i> Meu Perfil</a>
+                </li>
+                <li>
+                    <a href="{{ route('admins') }}" class="block py-2 px-4 hover:bg-gray-100"><i class="fas fa-users-cog"></i> Administradores</a>
+                </li>
+                <li>
+                    <a href="{{ route('viewConfig') }}" class="block py-2 px-4 hover:bg-gray-100"><i class="fas fa-cog"></i> Configurações</a>
+                </li>
             </ul>
             <div class="py-1">
-            <a href="{{ route('logout') }}" class="block py-2 px-4 text-sm c hover:bg-gray-100"><i class="fas fa-sign-out-alt"></i> Log-out</a>
+                <a href="{{ route('logout') }}" class="block py-2 px-4 text-sm c hover:bg-gray-100"><i class="fas fa-sign-out-alt"></i> Log-out</a>
             </div>
         </div>
     @endauth
