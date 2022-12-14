341cac10ed44f4066f9d2fe81586a69330798421
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-12-14 11:13:56 -0300

    texto do input

--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -1,5 +1,5 @@
 <input name="{{ $name }}" id="{{ $name }}" type="text" value="{{ $value }}" placeholder=" "
-    class="block py-2.5 px-0 w-full text-xl bg-transparent border-0 border-b-2 border-gray-300 focus:outline-none focus:border-gray-600 peer
+    class="block py-2.5 px-0 w-full text-base bg-transparent border-0 border-b-2 border-gray-300 focus:outline-none focus:border-gray-600 peer
                         @if ($errors->has("$name")) text-red-500 border-red-300 @endif" />
 <label for="{{ $name }}"
     class="whitespace-nowrap peer-focus:font-medium absolute text-xl duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8
